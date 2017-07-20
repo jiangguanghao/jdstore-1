0c6122ad8065bfc0bc99ad002b94efa0afe3510e
Your Name <you@example.com>
2017-07-20 11:55:35 +0800

    add aasm_state to order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,6 +16,7 @@ class OrdersController < ApplicationController
         product_list.quantity = cart_item.quantity
         product_list.save
       end
+
       current_cart.clean!
       OrderMailer.notify_order_placed(@order).deliver!
 
@@ -27,14 +28,14 @@ class OrdersController < ApplicationController
   end
 
   def show
-    @order = Order.find_by_token(params[:id])
+    @order = Order.find(params[:id])
     @product_lists = @order.product_lists
   end
 
   def pay_with_alipay
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("alipay")
-    @order.pay!
+    @order.make_payment!
 
     redirect_to order_path(@order.token), notice: "使用支付宝成功完成付款"
   end
@@ -42,7 +43,7 @@ class OrdersController < ApplicationController
   def pay_with_wechat
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("wechat")
-    @order.pay!
+    @order.meke_payment!
 
     redirect_to order_path(@order.token), notice: "使用微信成功完成付款"
   end
